0e6e678a65794def06fb6e74e62d0cb8da9c4437
Eugene <eugeniusratshipa@gmail.com>
2024-10-18 04:27:25 +0000

    Create entrypoint for database start

---
+++ backend/user-service/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Wait for the database to be ready
+wait-for-it.sh db:5432 -- echo "Postgres is up - executing command"
+
+# Run Prisma migrations
+npx prisma migrate deploy
+
+# Seed the database
+npx ts-node prisma/seed.ts
+
+# Start the application
+exec "$@"

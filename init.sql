cf12c2b723c40de76570756c7719230dabd434ba
Eugene <eugeniusratshipa@gmail.com>
2024-10-18 06:41:03 +0000

    db initialising

--- init.sql
+++ init.sql
@@ -1,3 +1,9 @@
-CREATE USER authentico_user WITH PASSWORD 'password';
-CREATE DATABASE authentico_users;
-GRANT ALL PRIVILEGES ON DATABASE authentico_users TO authentico_user;
+DO $$ BEGIN
+    CREATE ROLE authentico_user;
+EXCEPTION
+    WHEN duplicate_object THEN
+        RAISE NOTICE 'Role authentico_user already exists, skipping.';
+END $$;
+
+ALTER ROLE authentico_user WITH PASSWORD 'password';
+CREATE DATABASE authentico_users OWNER authentico_user;

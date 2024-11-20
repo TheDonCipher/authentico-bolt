4835e8af40e7f81eda5f885bb4ca54254558f6a4
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2024-11-20 11:39:12 +0200

    migration from flexbox to grid

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -23,6 +23,7 @@ import {
   useDisconnect,
   useActiveWallet,
 } from "thirdweb/react";
+import { BellIcon } from "app/svg";
 
 interface Document {
   id: number;
@@ -250,10 +251,6 @@ const Dashboard = () => {
     }; */
   // TODO:move the avatar to the top corner
   // TODO:write the light mode equivalent
-  // TODO:remove the whites
-  // TODO:round up the cards
-  // TODO:Statistics should be on the landing page
-  // TODO: remove the your docs tag
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col md:flex-row">
       {/* Sidebar */}
@@ -307,146 +304,158 @@ const Dashboard = () => {
           </button>
         </div>
       </aside>
+      <div className="grid grid-cols-4 grid-rows-8 h-screen">
+        <AvatarTab />
+        {/* Main Content */}
+        <main className="flex-grow p-4 col-start-1 row-start-2 col-end-5  md:p-8 relative">
+          {/* <h2 className="text-3xl md:text-4xl font-black mb-6 md:mb-8 bg-indigo-800 p-4 border-8 border-white inline-block transform -rotate-2"> */}
+          {/*   {activeTab.toUpperCase()} */}
+          {/* </h2> */}
 
-      {/* Main Content */}
-      <main className="flex-grow p-4 md:p-8 relative">
-        <h2 className="text-3xl md:text-4xl font-black mb-6 md:mb-8 bg-indigo-800 p-4 border-8 border-white inline-block transform -rotate-2">
-          {/* {activeTab.toUpperCase()} */}
-        </h2>
-
-        {activeTab === "documents" && (
-          <div className="bg-gray-800 p-4 md:p-6 border-8 border-white">
-            <Stats documents={documents} />
-
-            <h3 className="text-xl md:text-2xl font-black mb-4">
-              Your Documents
-            </h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
-              {documents.map((doc) => (
-                <div
-                  key={doc.id}
-                  className="bg-gray-700 p-6 border-4 border-white flex flex-col transition-all duration-300 hover:shadow-lg hover:scale-105"
-                >
-                  <div className="flex justify-between items-start mb-4">
-                    <span className="font-bold text-lg">{doc.name}</span>
-                    <StatusBadge
-                      status={doc.status as "verified" | "pending" | "rejected"}
-                    />
-                  </div>
-                  <p className="text-sm text-gray-300 mb-2">
-                    Verifying Org: {doc.verifyingOrg}
-                  </p>
-                  <div className="mb-4">
-                    <p className="text-sm mb-1">
-                      Similarity to common documents:
-                    </p>
-                    <div className="w-full bg-gray-600 rounded-full h-2.5">
-                      <div
-                        className="bg-blue-500 h-2.5 rounded-full"
-                        style={{ width: `${doc.similarity}%` }}
-                      ></div>
-                    </div>
-                    <p className="text-right text-sm mt-1">{doc.similarity}%</p>
-                  </div>
-                  <div className="flex flex-wrap gap-2 mt-auto">
-                    {doc.status === "pending" && (
-                      <button className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 transition flex items-center">
-                        <RefreshCw size={16} className="mr-2" /> Check Status
-                      </button>
-                    )}
-                    {doc.status === "verified" && (
-                      <button className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600 transition flex items-center">
-                        <Download size={16} className="mr-2" /> Download
-                      </button>
-                    )}
-                    {doc.status === "rejected" && (
-                      <>
-                        <button
-                          className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition flex items-center"
-                          onClick={() =>
-                            alert(`Rejection Reason: ${doc.rejectionReason}`)
-                          }
-                        >
-                          <Eye size={16} className="mr-2" /> View Reason
-                        </button>
-                        <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 transition flex items-center">
-                          <RefreshCw size={16} className="mr-2" /> Re-upload
-                        </button>
-                      </>
-                    )}
-                    <button
-                      className="bg-gray-500 text-white px-3 py-1 rounded hover:bg-gray-600 transition flex items-center ml-auto"
-                      onClick={() => {
-                        setSelectedDocument(doc);
-                        setIsShareDialogOpen(true);
-                      }}
+          {activeTab === "documents" && (
+            <div className="p-4 md:p-6  flex flex-col gap-8">
+              <Stats documents={documents} />
+              <div>
+                <h3 className="text-xl md:text-2xl font-black mb-4">
+                  Your Documents
+                </h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+                  {documents.map((doc) => (
+                    <div
+                      key={doc.id}
+                      className="bg-gray-700 p-6 border-4 border-white flex flex-col transition-all duration-300 hover:shadow-lg hover:scale-105"
                     >
-                      <Share2 size={16} className="mr-2" /> Share
-                    </button>
-                  </div>
+                      <div className="flex justify-between items-start mb-4">
+                        <span className="font-bold text-lg">{doc.name}</span>
+                        <StatusBadge
+                          status={
+                            doc.status as "verified" | "pending" | "rejected"
+                          }
+                        />
+                      </div>
+                      <p className="text-sm text-gray-300 mb-2">
+                        Verifying Org: {doc.verifyingOrg}
+                      </p>
+                      <div className="mb-4">
+                        <p className="text-sm mb-1">
+                          Similarity to common documents:
+                        </p>
+                        <div className="w-full bg-gray-600 rounded-full h-2.5">
+                          <div
+                            className="bg-blue-500 h-2.5 rounded-full"
+                            style={{ width: `${doc.similarity}%` }}
+                          ></div>
+                        </div>
+                        <p className="text-right text-sm mt-1">
+                          {doc.similarity}%
+                        </p>
+                      </div>
+                      <div className="flex flex-wrap gap-2 mt-auto">
+                        {doc.status === "pending" && (
+                          <button className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 transition flex items-center">
+                            <RefreshCw size={16} className="mr-2" /> Check
+                            Status
+                          </button>
+                        )}
+                        {doc.status === "verified" && (
+                          <button className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600 transition flex items-center">
+                            <Download size={16} className="mr-2" /> Download
+                          </button>
+                        )}
+                        {doc.status === "rejected" && (
+                          <>
+                            <button
+                              className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition flex items-center"
+                              onClick={() =>
+                                alert(
+                                  `Rejection Reason: ${doc.rejectionReason}`,
+                                )
+                              }
+                            >
+                              <Eye size={16} className="mr-2" /> View Reason
+                            </button>
+                            <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 transition flex items-center">
+                              <RefreshCw size={16} className="mr-2" /> Re-upload
+                            </button>
+                          </>
+                        )}
+                        <button
+                          className="bg-gray-500 text-white px-3 py-1 rounded hover:bg-gray-600 transition flex items-center ml-auto"
+                          onClick={() => {
+                            setSelectedDocument(doc);
+                            setIsShareDialogOpen(true);
+                          }}
+                        >
+                          <Share2 size={16} className="mr-2" /> Share
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </div>
-        )}
-
-        {activeTab === "stats" && (
-          <div className="bg-gray-800 p-6 border-8 border-white">
-            <h3 className="text-2xl font-black mb-4">Quick Stats</h3>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              <div className="bg-indigo-700 p-4 border-4 border-white">
-                <h4 className="font-bold mb-2">Total Documents</h4>
-                <p className="text-3xl font-black">{documents.length}</p>
-              </div>
-              <div className="bg-green-700 p-4 border-4 border-white">
-                <h4 className="font-bold mb-2">Verified Documents</h4>
-                <p className="text-3xl font-black">
-                  {documents.filter((doc) => doc.status === "verified").length}
-                </p>
-              </div>
-              <div className="bg-yellow-700 p-4 border-4 border-white">
-                <h4 className="font-bold mb-2">Pending Documents</h4>
-                <p className="text-3xl font-black">
-                  {documents.filter((doc) => doc.status === "pending").length}
-                </p>
               </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {activeTab === "activity" && (
-          <div className="bg-gray-800 p-6 border-8 border-white">
-            <h3 className="text-2xl font-black mb-4">Recent Activity</h3>
-            <ul className="space-y-2">
-              {activities.map((activity, index) => (
-                <li
-                  key={index}
-                  className="bg-gray-700 p-4 border-4 border-white flex items-center"
-                >
-                  <span className="mr-4">
-                    {activity.icon as React.ReactNode}
-                  </span>
-                  <div>
-                    <p className="font-bold">{activity.text as string}</p>
-                    <p className="text-sm text-gray-400">
-                      {activity.date as string}
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+          {activeTab === "stats" && (
+            <div className="bg-gray-800 p-6 border-8 border-white">
+              <h3 className="text-2xl font-black mb-4">Quick Stats</h3>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="bg-indigo-700 p-4 border-4 border-white">
+                  <h4 className="font-bold mb-2">Total Documents</h4>
+                  <p className="text-3xl font-black">{documents.length}</p>
+                </div>
+                <div className="bg-green-700 p-4 border-4 border-white">
+                  <h4 className="font-bold mb-2">Verified Documents</h4>
+                  <p className="text-3xl font-black">
+                    {
+                      documents.filter((doc) => doc.status === "verified")
+                        .length
+                    }
+                  </p>
+                </div>
+                <div className="bg-yellow-700 p-4 border-4 border-white">
+                  <h4 className="font-bold mb-2">Pending Documents</h4>
+                  <p className="text-3xl font-black">
+                    {documents.filter((doc) => doc.status === "pending").length}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
 
-        {/* Floating Add Document Button */}
-        <button
-          className="fixed bottom-8 right-8 bg-indigo-600 text-white p-4 rounded-full hover:bg-indigo-700 transition shadow-lg border-4 border-white"
-          onClick={() => setIsUploadDialogOpen(true)}
-        >
-          <Plus size={24} />
-        </button>
-      </main>
+          {activeTab === "activity" && (
+            <div className="bg-gray-800 p-6 border-8 border-white">
+              <h3 className="text-2xl font-black mb-4">Recent Activity</h3>
+              <ul className="space-y-2">
+                {activities.map((activity, index) => (
+                  <li
+                    key={index}
+                    className="bg-gray-700 p-4 border-4 border-white flex items-center"
+                  >
+                    <span className="mr-4">
+                      {activity.icon as React.ReactNode}
+                    </span>
+                    <div>
+                      <p className="font-bold">{activity.text as string}</p>
+                      <p className="text-sm text-gray-400">
+                        {activity.date as string}
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
 
+          {/* Floating Add Document Button */}
+          <button
+            className="fixed bottom-8 right-8 bg-indigo-600 text-white p-4 rounded-full hover:bg-indigo-700 transition shadow-lg border-4 border-white"
+            onClick={() => setIsUploadDialogOpen(true)}
+          >
+            <Plus size={24} />
+          </button>
+        </main>
+      </div>
       {/* Upload Dialog */}
       {isUploadDialogOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
@@ -516,24 +525,29 @@ interface Idocuments {
   documents: Array<Document>;
 }
 function Stats({ documents }: Idocuments) {
+  let verifiedDocsNum =
+    documents.filter((doc) => doc.status === "verified").length + "+";
+
   return (
-    <div className="bg-gray-800 p-6 border-8 border-white">
+    <div>
       <h3 className="text-2xl font-black mb-4">Quick Stats</h3>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="bg-indigo-700 p-4 border-4 border-white">
+        <div className="bg-indigo-700 p-4 rounded-md ">
           <h4 className="font-bold mb-2">Total Documents</h4>
-          <p className="text-3xl font-black">{documents.length}</p>
-        </div>
-        <div className="bg-green-700 p-4 border-4 border-white">
-          <h4 className="font-bold mb-2">Verified Documents</h4>
           <p className="text-3xl font-black">
-            {documents.filter((doc) => doc.status === "verified").length}
+            {documents.length > 0 ? documents.length + "+" : "0"}
           </p>
         </div>
-        <div className="bg-yellow-700 p-4 border-4 border-white">
+        <div className="bg-green-700 p-4 rounded-md ">
+          <h4 className="font-bold mb-2">Verified Documents</h4>
+          <p className="text-3xl font-black">
+            {documents.filter((doc) => doc.status === "verified").length + "+"}
+          </p>
+        </div>
+        <div className="bg-yellow-700 p-4 rounded-md ">
           <h4 className="font-bold mb-2">Pending Documents</h4>
           <p className="text-3xl font-black">
-            {documents.filter((doc) => doc.status === "pending").length}
+            {documents.filter((doc) => doc.status === "pending").length + "+"}
           </p>
         </div>
       </div>
@@ -541,4 +555,22 @@ function Stats({ documents }: Idocuments) {
   );
 }
 
+function AvatarTab() {
+  return (
+    <div className="bg-[#312e81] col-start-1 col-end-5 h-14 flex justify-end">
+      <div>
+        <div>
+          <BellIcon />
+        </div>
+        <div className="mb-8 text-center flex items-center ">
+          <div className="w-20 h-20 rounded-full bg-gray-700 mx-auto mb-2 flex items-center justify-center">
+            <User size={40} />
+          </div>
+          {/* <p className="font-bold">{formatAddress(activeAccount)}</p> */}
+          <p className="font-bold">Yandhi</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Dashboard;

d7e969df94d74777622a4fc2287f36311274f644
born2code265 <born2code265@gmail.com>
2025-01-30 06:26:38 +0200

    "please Read the Read Me in  Smart contracts workspace"

--- smart_contracts/hardhat.config.ts
+++ smart_contracts/hardhat.config.ts
@@ -49,6 +49,7 @@ const config: HardhatUserConfig = {
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
+      accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`],
     },
     sepolia: {
       url: "https://sepolia.infura.io/v3/fbe90be3403d47068b85a400a75fb456",

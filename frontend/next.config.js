aebaea1e09d26dcb5ebbd1d6e45cfe0c0f87ae8e
Eugene <eugeniusratshipa@gmail.com>
2025-04-20 17:01:14 +0200

    Added Firebase dependencies.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['thirdweb'],
+  transpilePackages: ['thirdweb', 'firebase'],
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
